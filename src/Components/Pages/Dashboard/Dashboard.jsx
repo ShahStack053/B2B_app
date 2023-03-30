66ab4f0883e63568f72bc1990a259c7a2f538c00
raza <shahhsoft@gmail.com>
2023-03-30 23:28:27 +0500

    widget set using grid and antd card

--- src/Components/Pages/Dashboard/Dashboard.jsx
+++ src/Components/Pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "../../../Assets/Images/logo.png";
 import userImage from "../../../Assets/Images/userImage.png";
+import { Card, Input } from "antd";
+
 import "./Dashboard.css";
 
 import {
@@ -12,7 +14,13 @@ import {
   ShoppingCartOutlined,
   ShopOutlined,
   SettingOutlined,
-  BellFilled,
+  SearchOutlined,
+  RiseOutlined,
+  BellOutlined,
+  ShopFilled,
+  FallOutlined,
+  DashOutlined,
+  ShoppingOutlined,
   LogoutOutlined,
 } from "@ant-design/icons";
 import { Menu } from "antd";
@@ -39,13 +47,13 @@ const finalDate = `${day} ${month}, ${year}`;
 //Side bar Item
 const items = [
   getItem("Dashboard", "1", <AppstoreOutlined />),
-  getItem("Products", "2", <TeamOutlined />),
+  getItem("Products", "2", <ShopOutlined />),
   getItem("Orders", "3", <ShoppingCartOutlined />),
   getItem("RFQs", "4", <FormOutlined />),
   getItem("Finances", "5", <BarChartOutlined />),
   getItem("Admins", "6", <TeamOutlined />),
-  getItem("Dark Stores", "7", <ShopOutlined />),
-  getItem("Business Customers", "8", <UserOutlined />),
+  getItem("Dark Stores", "7", <ShopFilled />),
+  getItem("Business Customers", "8", <TeamOutlined />),
   {
     type: "divider",
   },
@@ -69,30 +77,249 @@ const Dashboard = () => {
       </div>
       <div className="rightSideDiv">
         <div className="topBar">
-          <div className="topbarLeft">
-            <div className="searchLeftTop">
-              {/* <input type="text" placeholder="Search" value="" /> */}
-              <input type="text" name="" placeholder="Search..." />
-            </div>
-            <div className="dashboardTopBar">
-              <h3 className="dashboardTop">Dashboard</h3>
-            </div>
+          <div className="searchLeftTop">
+            <Input
+              style={{ borderRadius: 18, width: 265 }}
+              placeholder="Search..."
+              prefix={<SearchOutlined />}
+            />
+          </div>
+          <div className="dashboardTopBarDiv">
+            <h3 className="dashboardTop">Dashboard</h3>
           </div>
           <div className="topBarRight">
             <p className="DateTop">{finalDate}</p>
             <span className="iconbellTop">
-              <BellFilled />
+              <BellOutlined />
             </span>
-
             <img src={userImage} alt="userImage" className="userImage" />
             <select className="personDropDown">
               <option>Jamal</option>
             </select>
           </div>
         </div>
-        <div className="main">
-          <div className="topDiv"></div>
+        <div className="container">
+          <div className="main">
+            <Card className="graph-div">
+              <div className="graph-div-up">
+                <div className="graph-credential-div">
+                  <span className="graph-title"> Overall Orders</span>
+                  <p className="graph-price">
+                    SAR 238,560.93&nbsp;&nbsp;
+                    <span className="graph-percentage">
+                      <RiseOutlined />
+                      &nbsp; 13.02%
+                    </span>
+                  </p>
+                </div>
+                <div className="graph-month-div">
+                  <select className="graph-month">
+                    <option>This Month</option>
+                  </select>
+                </div>
+              </div>
+              <div className="graph">graph</div>
+            </Card>
+
+            <div className="widgets-order-div">
+              <Card className="widget-order-card">
+                <div className="widget-up">
+                  <span>
+                    <ShoppingOutlined />
+                  </span>
+                  <span>
+                    <DashOutlined />
+                  </span>
+                </div>
+                <div>
+                  <span className="card-title"> Completed Orders</span>
+                  <p className="card-orders"> 10,856</p>
+                  <p className="card-percentage-rise">
+                    <RiseOutlined />
+                    &nbsp; 13.02% &nbsp;
+                    <span className="card-month">From May</span>
+                  </p>
+                </div>
+              </Card>
+              <Card className="widget-order-card">
+                <div className="widget-up">
+                  <span>
+                    <ShoppingOutlined />
+                  </span>
+                  <span>
+                    <DashOutlined />
+                  </span>
+                </div>
+                <div>
+                  <span className="card-title">New Orders</span>
+                  <p className="card-orders"> 563</p>
+                  <p className="card-percentage-rise">
+                    <RiseOutlined />
+                    &nbsp; 3.02% &nbsp;
+                    <span className="card-month">From May</span>
+                  </p>
+                </div>
+              </Card>
+              <Card className="widget-order-card">
+                <div className="widget-up">
+                  <span>
+                    <ShoppingOutlined />
+                  </span>
+                  <span>
+                    <DashOutlined />
+                  </span>
+                </div>
+                <div>
+                  <span className="card-title"> Pending Orders</span>
+                  <p className="card-orders"> 23</p>
+                  <p className="card-percentage-rise">
+                    <RiseOutlined />
+                    &nbsp; 6.02%% &nbsp;
+                    <span className="card-month">From May</span>
+                  </p>
+                </div>
+              </Card>
+              <Card className="widget-order-card">
+                <div className="widget-up">
+                  <span>
+                    <ShoppingOutlined />
+                  </span>
+                  <span>
+                    <DashOutlined />
+                  </span>
+                </div>
+                <div>
+                  <span className="card-title"> Rejected Orders</span>
+                  <p className="card-orders"> 122</p>
+                  <p className="card-percentage-fall">
+                    <FallOutlined />
+                    &nbsp; 0.32% &nbsp;
+                    <span className="card-month">From May</span>
+                  </p>
+                </div>
+              </Card>
+            </div>
+          </div>
+          <div className="widgets-card-div">
+            <Card className="widget-card">
+              <div className="widget-up">
+                <span>
+                  <ShoppingOutlined />
+                </span>
+                <span>
+                  <DashOutlined />
+                </span>
+              </div>
+              <div>
+                <span className="card-title"> Total Order Value</span>
+                <p className="card-orders"> 18,560.93</p>
+                <p className="card-percentage-rise">
+                  <RiseOutlined /> &nbsp; 13.02% &nbsp;
+                  <span className="card-month">From May</span>
+                </p>
+              </div>
+            </Card>
+            <Card className="widget-card">
+              <div className="widget-up">
+                <span>
+                  <ShoppingOutlined />
+                </span>
+                <span>
+                  <DashOutlined />
+                </span>
+              </div>
+              <div>
+                <span className="card-title"> Open Order Value</span>
+                <p className="card-orders"> 564.34</p>
+                <p className="card-percentage-rise">
+                  <RiseOutlined />
+                  &nbsp; 3.02% &nbsp;
+                  <span className="card-month">From May</span>
+                </p>
+              </div>
+            </Card>
+            <Card className="widget-card">
+              <div className="widget-up">
+                <span>
+                  <ShoppingOutlined />
+                </span>
+                <span>
+                  <DashOutlined />
+                </span>
+              </div>
+              <div>
+                <span className="card-title"> Total Business Customer</span>
+                <p className="card-orders"> 8,560</p>
+                <p className="card-percentage-rise">
+                  <RiseOutlined />
+                  &nbsp; 13.02% &nbsp;
+                  <span className="card-month">From May</span>
+                </p>
+              </div>
+            </Card>
+            <Card className="widget-card">
+              <div className="widget-up">
+                <span>
+                  <ShoppingOutlined />
+                </span>
+                <span>
+                  <DashOutlined />
+                </span>
+              </div>
+              <div>
+                <span className="card-title"> Total BC Users</span>
+                <p className="card-orders"> 6,434</p>
+                <p className="card-percentage-rise">
+                  <RiseOutlined />
+                  &nbsp; 3.02% &nbsp;
+                  <span className="card-month">From May</span>
+                </p>
+              </div>
+            </Card>
+            <Card className="widget-card">
+              <div className="widget-up">
+                <span>
+                  <ShoppingOutlined />
+                </span>
+                <span>
+                  <DashOutlined />
+                </span>
+              </div>
+              <div>
+                <span className="card-title"> BC Pending Approval</span>
+                <p className="card-orders"> 34</p>
+                <p className="card-percentage-rise">
+                  <RiseOutlined />
+                  &nbsp; 3.02% &nbsp;
+                  <span className="card-month">From May</span>
+                </p>
+              </div>
+            </Card>
+          </div>
         </div>
+        {/* <div className="main">
+          <div className="top-main-div">
+            <div className="top-main-left">
+              <div className="top-main-left-up">
+                <div className="top-main-left-up-left">
+                  <p className="top-main-left-up-left-orders">Overall Orders</p>
+                  <div className="top-main-left-up-left-child">
+                    <p className="top-main-left-up-left-child-price">
+                      SAR 238,560.93 &nbsp;
+                    </p>
+                    <span className="top-main-left-up-left-child-percent">
+                      <RiseOutlined />
+                      13.02%
+                    </span>
+                  </div>
+                </div>
+                <div className="top-main-left-up-right">RightUp</div>
+              </div>
+              <div className="top-main-left-down">leftDown</div>
+            </div>
+            <div className="top-main-right">Right Main</div>
+          </div>
+        </div> */}
       </div>
     </div>
   );
